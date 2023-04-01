25c8467ba57c8ad92081c0109145727706034d20
Connor Bär <connor.baer@sumup.com>
2023-04-01 08:56:15 +0200

    Set default timezone for tests

--- jest.global-setup.js
+++ jest.global-setup.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019, SumUp Ltd.
+ * Copyright 2023, SumUp Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,13 +13,6 @@
  * limitations under the License.
  */
 
-module.exports = {
-  preset: 'ts-jest',
-  clearMocks: true,
-  rootDir: 'src',
-  coverageDirectory: '../__reports__',
-  reporters: ['default'],
-  coveragePathIgnorePatterns: ['node_modules'],
-  setupFilesAfterEnv: ['jest-extended/all', '../jest.setup.js'],
-  globalSetup: '../jest.global-setup.js',
+module.exports = async () => {
+  process.env.TZ = 'UTC';
 };
